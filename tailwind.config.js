f9d1c6936799dce37d595f6b8ab64c0f656369fd
Farhat Munib <mfarhatmunibfiaz@gmail.com>
2024-12-23 12:26:59 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",      // This will scan the index.html for class usage
+    "./src/**/*.{css,js}" // This will scan all CSS and JS files in src folder for Tailwind classes
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '475px',    // Extra small screen
+        'sm': '640px',    // Small screen
+        'md': '768px',    // Medium screen
+        'lg': '1024px',   // Large screen
+        'xl': '1280px',   // Extra large screen
+        '2xl': '1536px',  // Double extra large screen
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // Adding locally hosted Poppins font
+      },
+    },
+  },
+  plugins: [],
+};
