3d3ea74fb4f0cfd9bfcaa430015df912fd7bbd01
Farhat Munib <mfarhatmunibfiaz@gmail.com>
2024-12-23 14:17:28 +0500

    23122024 0217

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",      // This will scan the index.html for class usage
-    "./src/**/*.{css,js}" // This will scan all CSS and JS files in src folder for Tailwind classes
+    "./index.html",
+    "./src/**/*.{css,js}"
   ],
   theme: {
     extend: {
       screens: {
-        'xs': '475px',    // Extra small screen
-        'sm': '640px',    // Small screen
-        'md': '768px',    // Medium screen
-        'lg': '1024px',   // Large screen
-        'xl': '1280px',   // Extra large screen
-        '2xl': '1536px',  // Double extra large screen
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'], // Adding locally hosted Poppins font
+        poppins: ['Poppins', 'sans-serif'],
       },
     },
   },
